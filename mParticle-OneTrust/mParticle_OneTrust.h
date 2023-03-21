9eabb74ee09575a77b0963fc0ff961c12f67b475
Ben Baron <ben@einsteinx2.com>
2023-03-21 11:24:26 -0500

    feat: Add SPM support for iOS (#8)

--- mParticle-OneTrust/mParticle_OneTrust.h
+++ mParticle-OneTrust/mParticle_OneTrust.h
@@ -3,4 +3,8 @@
 FOUNDATION_EXPORT double mParticle_OneTrustVersionNumber;
 FOUNDATION_EXPORT const unsigned char mParticle_OneTrustVersionString[];
 
+#if defined(__has_include) && __has_include(<mParticle_OneTrust/MPKitOneTrust.h>)
 #import <mParticle_OneTrust/MPKitOneTrust.h>
+#else
+#import "MPKitOneTrust.h"
+#endif
