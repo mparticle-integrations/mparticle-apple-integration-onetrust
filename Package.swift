cee1fb43e0bfe00c9dd301ce6ad72d82a5847775
Ben Baron <bbaron@mparticle.com>
2024-05-03 15:05:26 -0500

    feat: Improve Privacy Manifest support

--- Package.swift
+++ Package.swift
@@ -4,16 +4,17 @@ import PackageDescription
 
 let package = Package(
     name: "mParticle-OneTrust",
-    platforms: [ .iOS(.v10) ],
+    platforms: [ .iOS(.v11) ],
     products: [
         .library(
             name: "mParticle-OneTrust",
             targets: ["mParticle-OneTrust"]),
     ],
     dependencies: [
-      .package(name: "mParticle-Apple-SDK",
+        .package(name: "mParticle-Apple-SDK",
                url: "https://github.com/mParticle/mparticle-apple-sdk",
-               .upToNextMajor(from: "8.0.0")),
+               .upToNextMajor(from: "8.22.0")),
+        // OneTrust's unique version formating makes automatic support up to the next major version no longer possible. Additionally, as a specific version is required in their UI for their SDK to function we do not include a specific version of the 'OTPublishersHeadlessSDK' here and expect the version to be defined in the client app.
     ],
     targets: [
         .target(
@@ -21,6 +22,7 @@ let package = Package(
             dependencies: ["mParticle-Apple-SDK"],
             path: "mParticle-OneTrust",
             exclude: ["Info.plist"],
+            resources: [.process("PrivacyInfo.xcprivacy")],
             publicHeadersPath: "."
         ),
     ]
